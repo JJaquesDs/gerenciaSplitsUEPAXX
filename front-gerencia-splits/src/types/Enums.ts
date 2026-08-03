07772e7ae2717493ffa66dc26e8874bf3f5233ca
Linda Nowaczyk <vitorianowaczyk@gmail.com>
2026-08-03 15:55:40 -0300

    feat: add enums de Período e Tipo de Manutenção

---
+++ front-gerencia-splits/src/types/Enums.ts
@@ -0,0 +1,13 @@
+export type PeriodoManutencao = 
+    "MENSAL" | 
+    "BIMESTRAL" | 
+    "TRIMESTRAL" | 
+    "SEMESTRAL" | 
+    "ANUAL"
+
+export type TipoManu =
+    "INSTALACAO" |
+    "DESINSTALACAO" |
+    "CORRETIVA" |
+    "PREVENTIVA" |
+    "INSTALACAO_PREVENTIVA"
